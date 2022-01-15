3fb9c08302222b5e7aeb2c85e136cc67b240ab88
helen3121433 <helen3121433@gmail.com>
2022-01-15 10:49:15 -0800

    update PyPoll.py with winner summary

--- PyPoll.py
+++ PyPoll.py
@@ -5,15 +5,22 @@ import os
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources","election_results.csv")
 
-# OPen the election results and read the file.
-
-#with open(file_to_load) as election_data:
-    #print the file object.
-    #print(election_data)
-
-
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# 1. Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options and candidate votes
+candidate_options = []
+
+# Declare the empty dictionary.
+candidate_votes = {}
+
+# Declare a varaible for winner candidate and winning count tracacker equal to zero
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -22,13 +29,81 @@ with open(file_to_load) as election_data:
     # rea the file object with the reader function
     file_reader = csv.reader(election_data)
 
-    #print each row in the csv file.
-    #for row in file_reader:
-     #   print(row)
-
     # print the header row.
     headers = next(file_reader)
-    print(headers)
+    #print(headers)
+
+    # print each row in the csv file.
+    for row in file_reader:
+        # print (row)
+        # 2. Add to the total vote count. total_votes  = total_votes + 1
+        total_votes += 1
+
+        # print the candidate name from each row.
+        candidate_name = row[2]
+
+        #If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            # Add candidate name to the lsit of candidates
+            candidate_options.append(candidate_name)
+            # Begin tracking that candidate's vote count
+            # we're setting each candidate's vote count to zero. 
+            # once we set it to zero, then we can start tallying the votes to each candidate
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count. 
+        # we put this out of the if statement but aligned with the for loop
+        # so that it will count is incremented as we iterate through each row
+        candidate_votes[candidate_name] +=1
+
+    # Determine the percentage of votes for ach candidate by lopping through the counts
+    # 1. Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        # 2. retrieve vote count of a candidate
+        votes = candidate_votes[candidate_name]
+        # 3. calculate the percentage of votes, and formulat in two decimal places
+        # "{:.2f}".format
+        vote_percentage = float(votes)/float(total_votes) * 100 
+
+        # 4. Print the candidate name and percentage of votes
+        #print out each candidate's name, vote count, and percentage of vote to the terminal.
+        # vote_percentage :.1f / in one decimal place
+        print(f"{candidate_name}: {vote_percentage:.1f} % ({votes:,})\n") 
+
+        # Determine winning vote count and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent = vote percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # and, set winning_candidate equal to candidate name
+            winning_candidate = candidate_name
+    
+    winning_candidate_summary = (
+        f"-------------------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning vote count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------------------\n"
+    )
+    print(winning_candidate_summary)
+
+
+
+# print the candidate vote dictionary
+print(candidate_votes)
+
+# print the candidate list.
+print(candidate_options)
+    
+# 3. print the total votes.
+print(total_votes)
+
+# Accumulator, count up all the votes by initialize a variable.
+# accoumulator will increment by 1 as we read each row in the for loop
+
+
+
 
 #using the open() function with the "w" mode we will warite data to the file
 #outfile = open(file_to_save,"w")
